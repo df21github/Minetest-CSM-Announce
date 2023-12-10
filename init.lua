17ffe03c4f180cc087f24476e1b954f5ed8d4e05
df21github <142984316+df21github@users.noreply.github.com>
2023-12-10 14:52:28 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,161 +1,81 @@
---[[
-   _   _   __    _   _   _____   ______   _____    _____
-  | | | | |  \  | | | | |  ___| |  __  | |  __ \  |  ___|
-  | | | | |   \ | | | | | |     | |  | | | |  \ \ | |___
-  | | | | | |\ \| | | | | |     | |  | | | |  | | |  ___|
-  | |_| | | | \   | | | | |___  | |__| | | |_ / / | |___
-  |_____| |_|  \__| |_| |_____| |______| |_____/  |_____|
-      ____     ____    ____    ______   _____   _____
-     |  _  \  / __ \  |  _  \ /  ____| |  ___| |  _  \
-     | |_| | | |__| | | |_| | | |____  | |___  | |_| |
-     |  ___/ |  __  | |    _/ \____  \ |  ___| |    _/
-     | |     | |  | | | |\ \   ____| | | |___  | |\ \
-     |_|     |_|  |_| |_| \_\ |______/ |_____| |_| \_\
+-- Name: announce
+-- Description: Displays customized text on the center of the screen on command
+-- Version: 1.2
+-- Author: Dr_Dinnerbone
 
-written by (C) 2018 Yaya MNH48 and contributors
-               released under The MIT License
---]]
+-- Define the mod namespace
+local modname = minetest.get_current_modname()
+local modpath = minetest.get_modpath(modname)
 
--- Load support for intllib.
--- local S, NS = dofile("/intllib.lua")
-local texttemp = ""    -- declare as empty string
-local serverprotocol   -- declare the name without type
+-- Default options
+local defaultColor = "FFFFFF"
+local defaultFont = "default"
+local defaultSize = 14
+local defaultDuration = 5
 
--- on connect to server, gets server protocol version
-minetest.register_on_mods_loaded(function()
-    serverprotocol = minetest.get_server_info().protocol_version
-end)
+-- Function to display customized text on the center of the screen
+local function displayCustomText(name, param)
+    -- Extract color, font, size, duration, and text information from the parameters
+    local color, font, size, duration, text = param:match("#([A-Fa-f0-9]+):(.-):(.-):(.-):(.-)$")
+    
+    -- Use default values if not specified
+    color = color or defaultColor
+    font = font or defaultFont
+    size = tonumber(size) or defaultSize
+    duration = tonumber(duration) or defaultDuration
+    text = text or param
+    
+    -- Display the customized text on the center of the screen with fading
+    minetest.display_chat_message({
+        message = text,
+        name = "",
+        color = tonumber(color, 16), -- Convert hex color to decimal
+        time = duration,
+        font = { name = font, size = size },
+    })
+end
 
--- command .uc for cli version
-minetest.register_chatcommand("uc", {
-	params = "<escapecode>",
-	description = "Directly convert and send out converted unicode escape.",
-	func = function(param)
-		texttemp = string.gsub(param, "\\u", "\\0x")
-        sendForProcess(texttemp,"cli")
-	end
+-- Register the new chat commands
+minetest.register_chatcommand("announce", {
+    params = "<text>",
+    description = "Display customized text on the center of the screen",
+    func = function(name, param)
+        displayCustomText(name, "#" .. defaultColor .. ":" .. defaultFont .. ":" .. defaultSize .. ":" .. defaultDuration .. ":" .. param)
+    end,
 })
 
--- command .ug for gui version
-minetest.register_chatcommand("ug", {
-	--description = S("Open the GUI to paste unicode escape, then press \"Say\" to send in chat."),
-	description = "Open the GUI to paste unicode escape, then press \"Say\" to send in chat.",
-    func = function(name, escuni)
-        minetest.show_formspec("unicodeparser:upgui",
-            "size[6,3]"..
-			"field[1,1;5,2;text;Input the unicode escape;]"..
-			"button_exit[2,2;2,1;say;Say]")
-    end
+minetest.register_chatcommand("announce_colour", {
+    params = "<colour>",
+    description = "Set the color for the .announce command (hex format)",
+    func = function(name, param)
+        defaultColor = param
+        minetest.chat_send_player(name, "Color set to #" .. defaultColor)
+    end,
 })
 
--- define what to do when gui accepted input
-minetest.register_on_formspec_input(function(formname, fields)
-    if formname == "unicodeparser:upgui" then
-        texttemp = string.gsub(fields.text, "\\u", "\\0x")
-        sendForProcess(texttemp, "gui")
-    elseif formname == "unicodeparser:uperr" then
-        return false
-    else
-        return true
-    end
-end)
+minetest.register_chatcommand("announce_duration", {
+    params = "<duration>",
+    description = "Set the default display duration for the .announce command",
+    func = function(name, param)
+        defaultDuration = tonumber(param) or defaultDuration
+        minetest.chat_send_player(name, "Duration set to " .. defaultDuration .. " seconds")
+    end,
+})
 
--- parse the input escaped strings
-function sendForProcess(texts,interface)
-    local allinput=half(texts,"\\")
-	if checkMessagePermission() == true then
-        displayError(interface,"Unfortunately, this server does not allow you to send chat message using CSM, so this mod will not work.")
-    else
-        local toProcess = {}
-        if allinput == nil then
-            displayError(interface,"Please input some text!")
-            return false
-        else
-            for i,line in ipairs(allinput) do
-                local lineTemp = utf8(tonumber(line))
-                table.insert(toProcess,lineTemp)
-            end
-            local finalOut = table.concat(toProcess)
-            if serverprotocol < 32 then
-                minetest.display_chat_message("You wrote: "..finalOut)
-            end
-            minetest.send_chat_message(finalOut)
-            return true
-        end
-    end
-end
+minetest.register_chatcommand("announce_size", {
+    params = "<size>",
+    description = "Set the default font size for the .announce command",
+    func = function(name, param)
+        defaultSize = tonumber(param) or defaultSize
+        minetest.chat_send_player(name, "Font size set to " .. defaultSize)
+    end,
+})
 
--- split the input escaped strings
-function half(inStr, inToken)
-    if inStr == nil then
-        return nil
-    else
-        local TableWord = {}
-        local fToken = "(.-)" .. inToken
-        local l_end = 1
-        local w, t, halfPos = inStr:find(fToken, 1)
-        while w do
-            if w ~= 1 or halfPos ~= "" then
-                table.insert(TableWord,halfPos)
-            end
-            l_end = t+1
-            w, t, halfPos = inStr:find(fToken, l_end)
-        end
-        if l_end <= #inStr then
-            halfPos = inStr:sub(l_end)
-            table.insert(TableWord, halfPos)
-        end
-        return TableWord
-    end
-end
-
--- convert the strings back to original unicode
-do
-    local string_char = string.char
-    function utf8(codep)
-        if codep < 128 then
-            return string_char(codep)
-        end
-        local s = ""
-        local max_pf = 32
-        while true do
-            local suffix = codep % 64
-            s = string_char(128 + suffix)..s
-            codep = (codep - suffix) / 64
-            if codep < max_pf then
-                return string_char((256 - (2 * max_pf)) + codep)..s
-            end
-            max_pf = max_pf / 2
-        end
-    end
-end
-
--- check if server allow sending message from CSM
-function checkMessagePermission()
-    local restr = minetest.get_csm_restrictions()["chat_messages"]
-    return restr
-end
-
--- display error according to message interface
-function displayError(interface,text)
-    if interface == "cli" then
-        print("[unicodeparser] ERROR: "..text)
-    elseif interface == "gui" then
-        minetest.after(1, showGui, text)
-    else
-        return false
-    end
-end
-
--- show the actual GUI
-function showGui(text)
-    minetest.show_formspec("unicodeparser:guierr",
-            "size[6,6]"..
-            "label[2.5,1;".."ERROR".."]"..
-            "textarea[1,2;5,4;;"..text..";]"..
-            "button_exit[2,5;2,1;ok;OK]")
-end
-
--- log the CSM
-local msg = "[unicodeparser] CSM loaded."
-minetest.log("info", msg)
+minetest.register_chatcommand("announce_font", {
+    params = "<filepath/to/font>",
+    description = "Set the default font file for the .announce command",
+    func = function(name, param)
+        defaultFont = param
+        minetest.chat_send_player(name, "Font set to " .. defaultFont)
+    end,
+})
